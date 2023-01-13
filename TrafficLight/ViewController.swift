e50929d56fd9ac14e62db2d2f0a0be5513a413b3
IvanTrukhin <trukhinivan25@gmail.com>
2023-01-13 16:35:33 +0500

    fixing constraints

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColour: UIView!
+    @IBOutlet var yellowColour: UIView!
+    @IBOutlet var greenColour: UIView!
+    
+    @IBOutlet var changeButtom: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redColour.layer.cornerRadius = 50
+        yellowColour.layer.cornerRadius = 50
+        greenColour.layer.cornerRadius = 50
+        changeButtom.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func controlButton() {
+    }
+    
 }
 
